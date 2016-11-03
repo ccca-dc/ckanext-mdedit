9f83c63cd29784120ab57508aa4ce6e8352e21f3
Anja <astemme@zaalpc34.zamg.ac.at>
2016-11-03 15:30:09 +0100

    Fixed Organization; Tags. Reminaing open issues: datepicker, link in help text, json File update, tests and integration with other extensions

--- ckanext/mdedit/helpers.py
+++ ckanext/mdedit/helpers.py
@@ -5,20 +5,32 @@ import pytz
 from pylons import config
 from pylons.i18n import gettext
 
+import ckan.logic as logic
+get_action = logic.get_action
+
+
 from ckanapi import LocalCKAN, NotFound, NotAuthorized
 """ Anja 29.9.2016 """
 import  ckan.plugins.toolkit as tk
 context = tk.c
 import ckan.lib.base as base
-c = base.c
+Base_c = base.c
 from pylons import c
 import logging
 log = logging.getLogger(__name__)
 """ Anja 29.9.2016 """
 
+def mdedit_get_org_name():
+
+    return ""
+
 def mdedit_get_name():
      # Get the user name of the logged-in user.
     user = c.userobj
+
+    log.debug(c)
+    log.debug(user)
+
     return user.fullname
 
 def mdedit_get_mail():
